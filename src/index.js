ab0544890dac3b172c4b900be78d0284f1cd0f68
Grzegorz Piejko <gp23.1982@gmail.com>
2023-09-26 20:11:04 +0200

    Commit testowy na gałęzi Grzegorz

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//test comment
+//nowy komentarz testowy
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
