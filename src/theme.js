94afca28adf09c8fec6a7bf42afcba514081c18d
Gracjana Kromska <gracjana0915@wp.pl>
2023-09-28 19:52:31 +0200

    added theme

--- src/theme.js
+++ src/theme.js
@@ -2,5 +2,10 @@ export const theme = {
     color: {
         black: "#000000",
         pattensBlue: "#D6E4FF",
+        mineShaft: "#333333",
+        mystic: "#E4E6F0",
+        woodSmoke: "#18181B",
+        waterloo: "#7E839A",
+        scienceBlue: "#0044CC",
     }
-}
+};
