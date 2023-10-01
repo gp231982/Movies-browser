3cb6596ec4bb0e5b71642e37ef9250ef86f24da9
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-01 19:43:01 +0200

    adding globalStyle

---
+++ src/core/App/index.js
@@ -0,0 +1,13 @@
+import { MainContent } from "../../features/MovieList/MainContent";
+import { GlobalStyle } from "./GlobalStyle";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
