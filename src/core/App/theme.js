1d0f780090b8983de8766731ada5b3d7ded914fb
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-03 17:32:44 +0200

    adding a head theme to the entire application

---
+++ src/core/App/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    color: {
+        whisper: "#F5F5FA",
+        black: "#000000",
+        pattensBlue: "#D6E4FF",
+        sail: "##ACC6FA",
+        mineShaft: "#333333",
+        mystic: "#E4E6F0",
+        woodSmoke: "#18181B",
+        waterloo: "#7E839A",
+        scienceBlue: "#0044CC",
+    },
+
+    breakpoint: {
+        mobile: "768px",
+    },
+};
