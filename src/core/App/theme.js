0461b75129ad7652686a7a04c3c26800eaacb2bb
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-03 18:47:38 +0200

    adding color and boxShadow

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,17 +1,23 @@
 export const theme = {
-    color: {
-        whisper: "#F5F5FA",
-        black: "#000000",
-        pattensBlue: "#D6E4FF",
-        sail: "##ACC6FA",
-        mineShaft: "#333333",
-        mystic: "#E4E6F0",
-        woodSmoke: "#18181B",
-        waterloo: "#7E839A",
-        scienceBlue: "#0044CC",
-    },
+  color: {
+    whisper: "#F5F5FA",
+    black: "#000000",
+    pattensBlue: "#D6E4FF",
+    sail: "##ACC6FA",
+    mineShaft: "#333333",
+    mystic: "#E4E6F0",
+    woodSmoke: "#18181B",
+    waterloo: "#7E839A",
+    scienceBlue: "#0044CC",
+    white: "#fff",
+  },
 
-    breakpoint: {
-        mobile: "768px",
-    },
+  breakpoint: {
+    mobile: "768px",
+  },
+
+  boxShadow: {
+    medium: "0px 4px 12px 0px rgba(186, 199, 213, 0.5)",
+    hover: "0px 10px 20px 0px rgba(186, 199, 213, 0.5)",
+  },
 };
