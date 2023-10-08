70393b50513c276bebf9fb725bb8b208d707c95d
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-08 20:37:13 +0200

    adding new apiURL

--- src/common/apiURLs.js
+++ src/common/apiURLs.js
@@ -1,5 +1,5 @@
+const apiKey = "4b8fe2faad502d0967a22b78268baad6";
 
-const apiKey = '4b8fe2faad502d0967a22b78268baad6'
+export const popularPeopleApiUrl = `https://api.themoviedb.org/3/person/popular?language=en-US&page=1&api_key=${apiKey}`;
 
- export const popularPeopleApiUrl = `https://api.themoviedb.org/3/person/popular?language=en-US&page=1&api_key=${apiKey}`;
- 
+export const popularMovieApiUrl = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${apiKey}`;
