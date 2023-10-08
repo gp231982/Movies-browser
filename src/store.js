0a0a85bb2de1894bd788c7d9c741b2442680f56c
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-08 20:37:02 +0200

    adding reducer movie and changing sagaMiddleware

--- src/store.js
+++ src/store.js
@@ -1,17 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import createSagaMiddleware from 'redux-saga';
-import peopleReducer from './slices/peopleSlice';
-import { watchFetchPeople } from './sagas/peopleSaga';
-
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import peopleReducer from "./slices/peopleSlice";
+import movieReducer from "./slices/movieSlice";
+import saga from "./core/App/saga";
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     people: peopleReducer,
+    movie: movieReducer,
   },
   middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(watchFetchPeople);
+sagaMiddleware.run(saga);
 
 export default store;
