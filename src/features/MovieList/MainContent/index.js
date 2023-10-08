16060718fff895fd0291ff4a5b30dc765eeadf26
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-08 20:38:23 +0200

    adding API to component MainContent

--- src/features/MovieList/MainContent/index.js
+++ src/features/MovieList/MainContent/index.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import poster from "./poster.png";
 import {
   Content,
@@ -15,148 +17,55 @@ import {
   Votes,
 } from "./styled";
 import { ReactComponent as RateIcon } from "./rate.svg";
+import {
+  fetchMovieRequest,
+  selectError,
+  selectLoading,
+  selectMovie,
+} from "../../../slices/movieSlice";
 
 export const MainContent = () => {
+  const dispatch = useDispatch();
+  const movies = useSelector(selectMovie);
+  console.log(movies);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchMovieRequest());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <Content>
       <SectionTile>Popular movies</SectionTile>
       <Wrapper>
-        <MovieWrapper>
-          <Picture src={poster} />
-          <Information>
-            <Tile>Mulan</Tile>
-            <Date>2020</Date>
-          </Information>
-          <TypesWrapper>
-            <Type>Action</Type>
-            <Type>Adventure</Type>
-            <Type>Drama</Type>
-          </TypesWrapper>
-          <Rating>
-            <RateIcon />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </MovieWrapper>
-        <MovieWrapper>
-          <Picture src={poster} />
-          <Information>
-            <Tile>Mulan</Tile>
-            <Date>2020</Date>
-          </Information>
-          <TypesWrapper>
-            <Type>Action</Type>
-            <Type>Adventure</Type>
-            <Type>Drama</Type>
-          </TypesWrapper>
-          <Rating>
-            <RateIcon />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </MovieWrapper>
-        <MovieWrapper>
-          <Picture src={poster} />
-          <Information>
-            <Tile>Mulan</Tile>
-            <Date>2020</Date>
-          </Information>
-          <TypesWrapper>
-            <Type>Action</Type>
-            <Type>Adventure</Type>
-            <Type>Drama</Type>
-          </TypesWrapper>
-          <Rating>
-            <RateIcon />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </MovieWrapper>
-        <MovieWrapper>
-          <Picture src={poster} />
-          <Information>
-            <Tile>Mulan</Tile>
-            <Date>2020</Date>
-          </Information>
-          <TypesWrapper>
-            <Type>Action</Type>
-            <Type>Adventure</Type>
-            <Type>Drama</Type>
-          </TypesWrapper>
-          <Rating>
-            <RateIcon />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </MovieWrapper>
-        <MovieWrapper>
-          <Picture src={poster} />
-          <Information>
-            <Tile>Mulan</Tile>
-            <Date>2020</Date>
-          </Information>
-          <TypesWrapper>
-            <Type>Action</Type>
-            <Type>Adventure</Type>
-            <Type>Drama</Type>
-          </TypesWrapper>
-          <Rating>
-            <RateIcon />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </MovieWrapper>
-        <MovieWrapper>
-          <Picture src={poster} />
-          <Information>
-            <Tile>Mulan</Tile>
-            <Date>2020</Date>
-          </Information>
-          <TypesWrapper>
-            <Type>Action</Type>
-            <Type>Adventure</Type>
-            <Type>Drama</Type>
-          </TypesWrapper>
-          <Rating>
-            <RateIcon />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </MovieWrapper>
-        <MovieWrapper>
-          <Picture src={poster} />
-          <Information>
-            <Tile>Mulan</Tile>
-            <Date>2020</Date>
-          </Information>
-          <TypesWrapper>
-            <Type>Action</Type>
-            <Type>Adventure</Type>
-            <Type>Drama</Type>
-          </TypesWrapper>
-          <Rating>
-            <RateIcon />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </MovieWrapper>
-        <MovieWrapper>
-          <Picture src={poster} />
-          <Information>
-            <Tile>Mulan</Tile>
-            <Date>2020</Date>
-          </Information>
-          <TypesWrapper>
-            <Type>Action</Type>
-            <Type>Adventure</Type>
-            <Type>Drama</Type>
-          </TypesWrapper>
-          <Rating>
-            <RateIcon />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </MovieWrapper>
+        {movies.map((movie) => (
+          <MovieWrapper key={movie.id}>
+            <Picture src={poster} />
+            <Information>
+              <Tile>{movie.title}</Tile>
+              <Date>2020</Date>
+            </Information>
+            <TypesWrapper>
+              <Type>Action</Type>
+              <Type>Adventure</Type>
+              <Type>Drama</Type>
+            </TypesWrapper>
+            <Rating>
+              <RateIcon />
+              <Rate>7,8</Rate>
+              <Votes>35 votes</Votes>
+            </Rating>
+          </MovieWrapper>
+        ))}
       </Wrapper>
     </Content>
   );
