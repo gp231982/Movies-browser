41e8d9de3c709cdb45ddfdf488f7a64afb542f9b
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-09 19:42:27 +0200

    adding new components genres and genre

---
+++ src/common/Genre/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Genre = styled.div`
+  background-color: ${({ theme }) => theme.color.mystic};
+  padding: 8px 16px 8px 16px;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.4;
+  text-align: left;
+`;
