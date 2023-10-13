c6e0576d2ab83e93bd8a31ecbce179f567273994
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-13 17:54:50 +0200

    space change

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -10,31 +10,31 @@ import {
 
 export const Pagination = () => {
   return (
-      <StyledPagination>
-          <Button>
-              <PreviousArrowIcon />
-              <PreviousArrowIcon />
-              <ButtonContent>First</ButtonContent>
-          </Button>
-          <Button>
-              <PreviousArrowIcon />
-              <ButtonContent>Previous</ButtonContent>
-          </Button>
-          <PagesCounter>
-              Page
-              <PageNumber>1</PageNumber>
-              of
-              <PageNumber>500</PageNumber>
-          </PagesCounter>
-          <Button>
-              <ButtonContent>Next</ButtonContent>
-              <NextArrowIcon />
-          </Button>
-          <Button>
-              <ButtonContent>Last</ButtonContent>
-              <NextArrowIcon />
-              <NextArrowIcon />
-          </Button>
-      </StyledPagination>
+    <StyledPagination>
+      <Button>
+        <PreviousArrowIcon />
+        <PreviousArrowIcon />
+        <ButtonContent>First</ButtonContent>
+      </Button>
+      <Button>
+        <PreviousArrowIcon />
+        <ButtonContent>Previous</ButtonContent>
+      </Button>
+      <PagesCounter>
+        Page
+        <PageNumber>1</PageNumber>
+        of
+        <PageNumber>500</PageNumber>
+      </PagesCounter>
+      <Button>
+        <ButtonContent>Next</ButtonContent>
+        <NextArrowIcon />
+      </Button>
+      <Button>
+        <ButtonContent>Last</ButtonContent>
+        <NextArrowIcon />
+        <NextArrowIcon />
+      </Button>
+    </StyledPagination>
   );
 };
