239123fbd153a40e7ba744e721a592a53723379c
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-14 21:43:31 +0200

    Remove MovieDetail and add MovieTile component to MovieDetails

--- src/features/MovieDetails/index.js
+++ src/features/MovieDetails/index.js
@@ -7,15 +7,39 @@ import {
 } from "../../slices/creditsSlice";
 
 import { useParams } from "react-router-dom";
+import { Genre } from "../../common/Genre";
+import { Genres } from "../../common/Genres";
+import {
+  Tile,
+  Picture,
+  Information,
+  Title,
+  Year,
+  ProductionAndRelease,
+  ProductionBox,
+  Production,
+  ProductionData,
+  ReleaseBox,
+  Release,
+  ReleaseData,
+  Description,
+  Rating,
+  Rate,
+  TotalRate,
+  Votes,
+} from "./styled";
+import { ReactComponent as RateIcon } from "../../common/MovieTile/rate.svg";
 
 import { fetchCreditsRequest } from "../../slices/creditsSlice";
 import { MoviePeopleWrapper, TilesWrapper } from "../MoviePeople/styled";
 import { selectMovie, selectMovieId } from "../../slices/movieSlice";
 import { Pagination } from "../../common/Pagination";
-import { SectionTile } from "../MovieList/styled";
+import { SectionTile, Wrapper } from "../MovieList/styled";
 import Header from "../MoviePeople/Header";
 import headerName from "../MoviePeople/Header";
 import PersonTile from "../../common/PersonTile";
+import { Loading } from "../../common/States/Loading";
+import { Error } from "../../common/States/Error";
 
 const MovieDetails = () => {
   const selectedMovieId = useParams();
@@ -36,11 +60,11 @@ const MovieDetails = () => {
   }, [dispatch, selectedMovieId]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <Error />;
   }
 
   if (!credits || !Array.isArray(credits.cast)) {
@@ -49,6 +73,41 @@ const MovieDetails = () => {
 
   return (
     <>
+      <Tile>
+        <Picture />
+        <Information>
+          <Title>Mulan</Title>
+          <Year>2023</Year>
+          <ProductionAndRelease>
+            <ProductionBox>
+              <Production>Production:</Production>
+              <ProductionData>China, United States of America</ProductionData>
+            </ProductionBox>
+            <ReleaseBox>
+              <Release>Release date:</Release>
+              <ReleaseData>24.10.2023</ReleaseData>
+            </ReleaseBox>
+          </ProductionAndRelease>
+          <Rating>
+            <RateIcon />
+            <Rate>9</Rate>
+            <TotalRate>/ 10</TotalRate>
+            <Votes>500 votes</Votes>
+          </Rating>
+          <Genres>
+            <Genre>Horror</Genre>
+            <Genre>Horror</Genre>
+            <Genre>Horror</Genre>
+          </Genres>
+          <Description>
+            A young Chinese maiden disguises herself as a male warrior in order
+            to save her father. Disguises herself as a male warrior in order to
+            save her father. A young Chinese maiden disguises herself as a male
+            warrior in order to save her father.
+          </Description>
+        </Information>
+      </Tile>
+
       <MoviePeopleWrapper>
         <SectionTile>Cast</SectionTile>
         <TilesWrapper>
