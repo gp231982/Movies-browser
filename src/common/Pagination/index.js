06d357ceab75dddfa0614fc077fe46e316823d15
Gracjana Kromska <gracjana0915@wp.pl>
2023-10-14 21:21:58 +0200

    changes in Pagination

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,37 +1,48 @@
 import {
-  StyledPagination,
-  Button,
-  ButtonContent,
-  PreviousArrowIcon,
-  NextArrowIcon,
-  PagesCounter,
-  PageNumber,
+    StyledPagination,
+    Button,
+    ButtonContent,
+    PreviousArrowIcon,
+    NextArrowIcon,
+    PagesCounter,
+    PageNumber,
 } from "./styled";
 import { usePagination } from "./usePagination";
 
 export const Pagination = () => {
+    const {
+        page,
+        allPages,
+        setNextPage,
+        setPreviousPage,
+        setFirstPage,
+        setLastPage,
+    } = usePagination();
+    const firstPage = page === 1;
+    const lastPage = page === allPages;
+
     return (
         <StyledPagination>
-            <Button>
+            <Button onClick={setFirstPage} disabled={firstPage}>
                 <PreviousArrowIcon />
                 <PreviousArrowIcon />
                 <ButtonContent>First</ButtonContent>
             </Button>
-            <Button>
+            <Button onClick={setPreviousPage} disabled={firstPage}>
                 <PreviousArrowIcon />
                 <ButtonContent>Previous</ButtonContent>
             </Button>
             <PagesCounter>
                 Page
-                <PageNumber>1</PageNumber>
+                <PageNumber>{page}</PageNumber>
                 of
-                <PageNumber>500</PageNumber>
+                <PageNumber>{allPages}</PageNumber>
             </PagesCounter>
-            <Button>
+            <Button onClick={setNextPage} disabled={lastPage}>
                 <ButtonContent>Next</ButtonContent>
                 <NextArrowIcon />
             </Button>
-            <Button>
+            <Button onClick={setLastPage} disabled={lastPage}>
                 <ButtonContent>Last</ButtonContent>
                 <NextArrowIcon />
                 <NextArrowIcon />
