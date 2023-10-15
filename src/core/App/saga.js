fa5eca88154f6584f6acd182a7f564731eb77eb4
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-15 23:38:40 +0200

    adding watchFetchMovieDetails to saga

--- src/core/App/saga.js
+++ src/core/App/saga.js
@@ -2,8 +2,13 @@ import { all } from "redux-saga/effects";
 import { watchFetchMovies } from "../../sagas/movieSaga";
 import { watchFetchPeople } from "../../sagas/peopleSaga";
 import { watchFetchCredits } from "../../sagas/creditsSaga";
+import { watchFetchMovieDetails } from "../../sagas/movieDetailSaga";
 
 export default function* saga() {
-  yield all([watchFetchMovies(), watchFetchPeople(), watchFetchCredits()]);
+  yield all([
+    watchFetchMovies(),
+    watchFetchPeople(),
+    watchFetchCredits(),
+    watchFetchMovieDetails(),
+  ]);
 }
-  
