439e617328b2c8b44e525399417934f18e0421c6
Grzegorz Piejko <gp23.1982@gmail.com>
2023-10-16 00:16:56 +0200

    Modify of cathalogue structure, add searching for movies by dynamic change of Url parameters.

--- src/slices/movieSlice.js
+++ src/slices/movieSlice.js
@@ -1,50 +1,65 @@
-  import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-  const movieSlice = createSlice({
-    name: "movie",
-    initialState: {
-      movies: [],
-      loading: false,
-      error: null,
-      genres: [],
-      movieId: null,
+const movieSlice = createSlice({
+  name: "movie",
+  initialState: {
+    movies: [],
+    loading: false,
+    error: null,
+    genres: [],
+    movieId: null,
+  },
+  reducers: {
+    fetchMovieRequest: (state) => {
+      state.loading = true;
+      state.error = null;
     },
-    reducers: {
-      fetchMovieRequest: (state) => {
-        state.loading = true;
-        state.error = null;
-      },
-      fetchMovieSuccess: (state, action) => {
-        state.loading = false;
-        state.movies = action.payload;
-      },
-      fetchMovieFailure: (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      },
-      fetchMovieGenre: (state, action) => {
-        state.loading = false;
-        state.genres = action.payload;
-      },
-      handleMovieClick: (state, action) => {
-        state.movieId = action.payload;
-      },
+    fetchMovieSuccess: (state, action) => {
+      state.loading = false;
+      state.movies.push(...action.payload);
     },
-  });
+    fetchMovieFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    fetchMovieGenre: (state, action) => {
+      state.loading = false;
+      state.genres = action.payload;
+    },
+    handleMovieClick: (state, action) => {
+      state.movieId = action.payload;
+    },
+    fetchAllMoviesSuccess: (state, action) => {
+      state.loading = false;
+      state.movies = action.payload;
+    },
+  },
+});
 
-  const selectMovieState = (state) => state.movie;
-  export const selectMovie = (state) => selectMovieState(state).movies;
-  export const selectLoading = (state) => selectMovieState(state).loading;
-  export const selectError = (state) => selectMovieState(state).error;
-  export const selectGenre = (state) => selectMovieState(state).genres;
-  export const selectMovieId = (state) => selectMovieState(state).movieId;
+export const selectMoviesByQuery = (state, query) => {
+  const movies = selectMovie(state);
+  if (!query || query.trim() === "") {
+    return movies;
+  }
+  return selectMovie(state).filter((movie) =>
+    movie.title.toUpperCase().includes(query.trim().toUpperCase())
+  );
+};
 
-  export const {
-    fetchMovieRequest,
-    fetchMovieSuccess,
-    fetchMovieFailure,
-    fetchMovieGenre,
-    handleMovieClick,
-  } = movieSlice.actions;
+const selectMovieState = (state) => state.movie;
+export const selectMovie = (state) => selectMovieState(state).movies;
+export const selectLoading = (state) => selectMovieState(state).loading;
+export const selectError = (state) => selectMovieState(state).error;
+export const selectGenre = (state) => selectMovieState(state).genres;
+export const selectMovieId = (state) => selectMovieState(state).movieId;
 
-  export default movieSlice.reducer;
+export const {
+  fetchMovieRequest,
+  fetchMovieSuccess,
+  fetchMovieFailure,
+  fetchMovieGenre,
+  handleMovieClick,
+  fetchAllMoviesSuccess,
+} = movieSlice.actions;
+
+export default movieSlice.reducer;
