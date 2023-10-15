a157609068cae11468b466f08188645647c6ed9b
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-15 23:41:58 +0200

    automatic space change

--- src/slices/movieSlice.js
+++ src/slices/movieSlice.js
@@ -1,50 +1,50 @@
-  import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-  const movieSlice = createSlice({
-    name: "movie",
-    initialState: {
-      movies: [],
-      loading: false,
-      error: null,
-      genres: [],
-      movieId: null,
+const movieSlice = createSlice({
+  name: "movie",
+  initialState: {
+    movies: [],
+    loading: false,
+    error: null,
+    genres: [],
+    movieId: null,
+  },
+  reducers: {
+    fetchMovieRequest: (state) => {
+      state.loading = true;
+      state.error = null;
     },
-    reducers: {
-      fetchMovieRequest: (state) => {
-        state.loading = true;
-        state.error = null;
-      },
-      fetchMovieSuccess: (state, action) => {
-        state.loading = false;
-        state.movies = action.payload;
-      },
-      fetchMovieFailure: (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      },
-      fetchMovieGenre: (state, action) => {
-        state.loading = false;
-        state.genres = action.payload;
-      },
-      handleMovieClick: (state, action) => {
-        state.movieId = action.payload;
-      },
+    fetchMovieSuccess: (state, action) => {
+      state.loading = false;
+      state.movies = action.payload;
     },
-  });
+    fetchMovieFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    fetchMovieGenre: (state, action) => {
+      state.loading = false;
+      state.genres = action.payload;
+    },
+    handleMovieClick: (state, action) => {
+      state.movieId = action.payload;
+    },
+  },
+});
 
-  const selectMovieState = (state) => state.movie;
-  export const selectMovie = (state) => selectMovieState(state).movies;
-  export const selectLoading = (state) => selectMovieState(state).loading;
-  export const selectError = (state) => selectMovieState(state).error;
-  export const selectGenre = (state) => selectMovieState(state).genres;
-  export const selectMovieId = (state) => selectMovieState(state).movieId;
+const selectMovieState = (state) => state.movie;
+export const selectMovie = (state) => selectMovieState(state).movies;
+export const selectLoading = (state) => selectMovieState(state).loading;
+export const selectError = (state) => selectMovieState(state).error;
+export const selectGenre = (state) => selectMovieState(state).genres;
+export const selectMovieId = (state) => selectMovieState(state).movieId;
 
-  export const {
-    fetchMovieRequest,
-    fetchMovieSuccess,
-    fetchMovieFailure,
-    fetchMovieGenre,
-    handleMovieClick,
-  } = movieSlice.actions;
+export const {
+  fetchMovieRequest,
+  fetchMovieSuccess,
+  fetchMovieFailure,
+  fetchMovieGenre,
+  handleMovieClick,
+} = movieSlice.actions;
 
-  export default movieSlice.reducer;
+export default movieSlice.reducer;
