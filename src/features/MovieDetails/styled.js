4f347c501280bd809f969033f28a7a1919137285
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-20 20:17:16 +0200

    adding media queries for mobile

--- src/features/MovieDetails/styled.js
+++ src/features/MovieDetails/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { ReactComponent as RateIc } from "../../common/MovieTile/rate.svg";
 
 export const StyledMoviePage = styled.div`
   width: 100%;
@@ -14,19 +15,19 @@ export const StyledMoviePage = styled.div`
 `;
 
 export const BigPoster = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    
-    img {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  img {
     width: 72%;
     height: auto;
     object-fit: cover;
-    }
+  }
 
-    &::before {
+  &::before {
     content: "";
     position: absolute;
     top: 0;
@@ -34,40 +35,40 @@ export const BigPoster = styled.div`
     width: 100%;
     height: 100%;
     background-image: linear-gradient(
-        189.44deg, 
-        rgba(0, 0, 0, 0) 58.48%, 
-        rgba(0, 0, 0, 0.106473) 63.17%, 
-        rgba(0, 0, 0, 0.235359) 68.85%, 
-        rgba(0, 0, 0, 0.492821) 78.08%, 
-        rgba(0, 0, 0, 0.740286) 85.86%, 
+        189.44deg,
+        rgba(0, 0, 0, 0) 58.48%,
+        rgba(0, 0, 0, 0.106473) 63.17%,
+        rgba(0, 0, 0, 0.235359) 68.85%,
+        rgba(0, 0, 0, 0.492821) 78.08%,
+        rgba(0, 0, 0, 0.740286) 85.86%,
         #000000 92.87%
-    ),
-    linear-gradient(
-        270deg, 
-        #000000 14.11%, 
-        rgba(0, 0, 0, 0.873268) 15.08%, 
-        rgba(0, 0, 0, 0.720529) 16.51%, 
-        rgba(0, 0, 0, 0.294577) 19.99%, 
-        rgba(0, 0, 0, 0.159921) 21.88%, 
+      ),
+      linear-gradient(
+        270deg,
+        #000000 14.11%,
+        rgba(0, 0, 0, 0.873268) 15.08%,
+        rgba(0, 0, 0, 0.720529) 16.51%,
+        rgba(0, 0, 0, 0.294577) 19.99%,
+        rgba(0, 0, 0, 0.159921) 21.88%,
         rgba(0, 0, 0, 0) 25.68%
-    ),
-    linear-gradient(
-        180deg, 
-        #000000 0%, 
-        rgba(0, 0, 0, 0.689555) 4.66%, 
-        rgba(0, 0, 0, 0.439033) 9.34%, 
-        rgba(0, 0, 0, 0.20628) 15.16%, 
+      ),
+      linear-gradient(
+        180deg,
+        #000000 0%,
+        rgba(0, 0, 0, 0.689555) 4.66%,
+        rgba(0, 0, 0, 0.439033) 9.34%,
+        rgba(0, 0, 0, 0.20628) 15.16%,
         rgba(0, 0, 0, 0) 24.22%
-    ),
-    linear-gradient(
-        90deg, 
-        #000000 13.6%, 
-        rgba(0, 0, 0, 0.984059) 14.58%, 
-        rgba(0, 0, 0, 0.967732) 15.44%, 
-        rgba(0, 0, 0, 0.865569) 16.3%, 
-        rgba(0, 0, 0, 0.230315) 22.87%, 
+      ),
+      linear-gradient(
+        90deg,
+        #000000 13.6%,
+        rgba(0, 0, 0, 0.984059) 14.58%,
+        rgba(0, 0, 0, 0.967732) 15.44%,
+        rgba(0, 0, 0, 0.865569) 16.3%,
+        rgba(0, 0, 0, 0.230315) 22.87%,
         rgba(0, 0, 0, 0) 26.64%
-    );
+      );
   }
 `;
 
@@ -160,60 +161,93 @@ export const BackgroundVotes = styled.div`
 
 export const Tile = styled.div`
   background-color: ${({ theme }) => theme.color.white};
-  padding: 40px;
   box-shadow: ${({ theme }) => theme.boxShadow.medium};
-  gap: 40px;
   display: grid;
-  grid-template-columns: auto 1fr;
-  margin-bottom: 20px;
-  width: 1368px;
-  height: 464;
+  grid-template-columns: 312px auto;
+  grid-template-rows: auto auto;
+  gap: 40px;
   padding: 40px;
+  max-width: 1368px;
   margin: 0 auto;
-  margin-bottom: 64px;
-  margin-top: 64px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-template-columns: 114px auto;
+    gap: 16px;
+    padding: 16px;
+  }
 `;
 
 export const Picture = styled.img`
+  grid-row: 1 /3;
   width: 312px;
   height: 464px;
   border-radius: 5px;
-  align-self: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-row: 1;
+    width: 114px;
+    height: 169px;
+  }
 `;
 
-export const Information = styled.div`
+export const Main = styled.div`
   display: flex;
-  padding: 8px 0px;
   flex-direction: column;
-  align-items: flex-start;
   gap: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-row: 1;
+    grid-column: 2;
+    gap: 8px;
+  }
 `;
 
 export const Title = styled.h1`
   font-size: 36px;
   font-weight: 600;
   line-height: 1.2;
+  text-align: left;
   color: ${({ theme }) => theme.color.black};
-  margin: 0px;
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1.3;
+    margin: 0;
+  }
 `;
 
 export const Year = styled.p`
   font-size: 22px;
   font-weight: 400;
   line-height: 1.2;
+  text-align: left;
   color: ${({ theme }) => theme.color.black};
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 13px;
+    margin: 0;
+  }
 `;
 
 export const ProductionAndRelease = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.2;
+  text-align: left;
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 12px;
+    line-height: 2;
+  }
 `;
 
 export const ProductionBox = styled.div`
   display: flex;
   gap: 10px;
-  margin: 0 0 8px 0;
 `;
 
 export const Production = styled.p`
@@ -222,6 +256,10 @@ export const Production = styled.p`
   line-height: 1.2;
   color: ${({ theme }) => theme.color.stormGray};
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    display: none;
+  }
 `;
 
 export const ProductionData = styled.p`
@@ -230,6 +268,12 @@ export const ProductionData = styled.p`
   line-height: 1.2;
   color: ${({ theme }) => theme.color.black};
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 1.3;
+  }
 `;
 
 export const ReleaseBox = styled.div`
@@ -244,6 +288,10 @@ export const Release = styled.p`
   line-height: 1.2;
   color: ${({ theme }) => theme.color.stormGray};
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    display: none;
+  }
 `;
 
 export const ReleaseData = styled.p`
@@ -252,6 +300,13 @@ export const ReleaseData = styled.p`
   line-height: 1.2;
   color: ${({ theme }) => theme.color.black};
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.3;
+  }
 `;
 
 export const Description = styled.p`
@@ -259,16 +314,23 @@ export const Description = styled.p`
   font-size: 20px;
   font-weight: 400;
   line-height: 1.6;
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.6;
+    grid-column: 1 / 3;
+    max-width: 288px;
+    margin: 0 0 0 -130px;
+  }
 `;
 
 export const Rating = styled.div`
-  align-self: flex-start;
-  align-items: center;
   display: flex;
-  flex-direction: row;
-  column-gap: 12px;
-  height: 24px;
-  margin: 16px 0 0 0;
+  gap: 8px;
+  text-align: left;
+  align-items: baseline;
 `;
 
 export const Rate = styled.p`
@@ -276,7 +338,10 @@ export const Rate = styled.p`
   font-size: 22px;
   font-weight: 500;
   line-height: 1.3;
-  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 13px;
+  }
 `;
 
 export const TotalRate = styled.p`
@@ -285,6 +350,10 @@ export const TotalRate = styled.p`
   font-weight: 400;
   line-height: 1.2;
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    display: none;
+  }
 `;
 
 export const Votes = styled.p`
@@ -292,7 +361,17 @@ export const Votes = styled.p`
   font-size: 14px;
   font-weight: 400;
   line-height: 1.2;
-  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 13px;
+    font-weight: 400;
+    line-height: 1.3;
+  }
 `;
 
-
+export const RateIcon = styled(RateIc)`
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    width: 16px;
+    height: 16px;
+  }
+`;
