9c7d210d877b970cc1c6855df3e39a4f09fa75b4
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-21 14:04:16 +0200

    Modify and add the ability to add an API to the personDetails component

--- src/features/PersonDetails/index.js
+++ src/features/PersonDetails/index.js
@@ -1,58 +1,69 @@
-import { CastPerson, CrewPerson, MovieList, StyledPersonDetails, Title } from "./styled";
-import { MovieTile } from "../../common/MovieTile";
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPersonId, selectPersonDetails, selectPersonError, selectPersonLoading } from "../../slices/personDetailSlice";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  CastPerson,
+  CrewPerson,
+  MovieList,
+  StyledPersonDetails,
+  Title,
+} from "./styled";
+import { MovieTile } from "../../common/MovieTile";
+import {
+  fetchPersonDetailsRequest,
+  selectPersonDetails,
+  selectPersonError,
+  selectPersonLoading,
+} from "../../slices/personDetailSlice";
+import {
+  fetchPersonCreditsRequest,
+  selectData,
+} from "../../slices/personCreditsSlice";
 import { Loading } from "../../common/States/Loading";
 import { Error } from "../../common/States/Error";
+import { selectPersonId } from "../../slices/peopleSlice";
 
 export const PersonDetails = () => {
-    const id = useParams().id;
-    const dispatch = useDispatch();
-    const loading = useSelector(selectPersonLoading);
-    const error = useSelector(selectPersonError);
-    const details = useSelector(selectPersonDetails);
+  const selectedPersonId = useSelector(selectPersonId);
+  const dispatch = useDispatch();
+  const credits = useSelector(selectData);
+  const loading = useSelector(selectPersonLoading);
+  const error = useSelector(selectPersonError);
+  const details = useSelector(selectPersonDetails);
+  console.log(credits);
+  console.log(details);
 
-    useEffect(() => {
-        if (location.includes()) dispatch(fetchPersonId(id));
-    }, [dispatch, location, id]);
-
-    if (loading) {
-        return <Loading />;
+  useEffect(() => {
+    if (selectedPersonId) {
+      dispatch(fetchPersonCreditsRequest(selectedPersonId));
+      dispatch(fetchPersonDetailsRequest(selectedPersonId));
     }
+  }, [dispatch, selectedPersonId]);
 
-    if (error) {
-        return <Error />;
-    }
+  if (loading) {
+    return <Loading />;
+  }
 
-    return (
-        <>
-            <StyledPersonDetails>
-                <>
-                    poster
-                    name
-                    birthday
-                    birthplace
-                    biography
-                </>
-                <CastPerson>
-                    <Title>
-                        Movies - cast
-                    </Title>
-                    <MovieList>
-                        <MovieTile />
-                    </MovieList>
-                </CastPerson>
-                <CrewPerson>
-                    <Title>
-                        Movies - crew
-                    </Title>
-                    <MovieList>
-                        <MovieTile />
-                    </MovieList>
-                </CrewPerson>
-            </StyledPersonDetails>
-        </>
-    )
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <>
+      <StyledPersonDetails>
+        <>{details.biography}</>
+        <CastPerson>
+          <Title>Movies - cast</Title>
+          <MovieList>
+            <MovieTile />
+          </MovieList>
+        </CastPerson>
+        <CrewPerson>
+          <Title>Movies - crew</Title>
+          <MovieList>
+            <MovieTile />
+          </MovieList>
+        </CrewPerson>
+      </StyledPersonDetails>
+    </>
+  );
 };
