522b469ba3ca76056dbfbb6dc5fda270ac6aa9be
Gracjana Kromska <gracjana0915@wp.pl>
2023-10-22 20:14:11 +0200

    added Person Tile to styled and index.js

--- src/features/PersonDetails/index.js
+++ src/features/PersonDetails/index.js
@@ -1,69 +1,94 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  CastPerson,
-  CrewPerson,
-  MovieList,
-  StyledPersonDetails,
-  Title,
+    Biography,
+    CastPerson,
+    CrewPerson,
+    Main,
+    MovieList,
+    PersonBirthData,
+    PersonBirthDetails,
+    PersonInfo,
+    PersonTile,
+    PersonTitle,
+    Picture,
+    StyledPersonDetails,
+    Title,
 } from "./styled";
 import { MovieTile } from "../../common/MovieTile";
 import {
-  fetchPersonDetailsRequest,
-  selectPersonDetails,
-  selectPersonError,
-  selectPersonLoading,
+    fetchPersonDetailsRequest,
+    selectPersonDetails,
+    selectPersonError,
+    selectPersonLoading,
 } from "../../slices/personDetailSlice";
 import {
-  fetchPersonCreditsRequest,
-  selectData,
+    fetchPersonCreditsRequest,
+    selectData,
 } from "../../slices/personCreditsSlice";
 import { Loading } from "../../common/States/Loading";
 import { Error } from "../../common/States/Error";
 import { selectPersonId } from "../../slices/peopleSlice";
+import { basicImageUrl } from "../MovieList";
 
 export const PersonDetails = () => {
-  const selectedPersonId = useSelector(selectPersonId);
-  const dispatch = useDispatch();
-  const credits = useSelector(selectData);
-  const loading = useSelector(selectPersonLoading);
-  const error = useSelector(selectPersonError);
-  const details = useSelector(selectPersonDetails);
-  console.log(credits);
-  console.log(details);
+    const selectedPersonId = useSelector(selectPersonId);
+    const dispatch = useDispatch();
+    const credits = useSelector(selectData);
+    const loading = useSelector(selectPersonLoading);
+    const error = useSelector(selectPersonError);
+    const details = useSelector(selectPersonDetails);
+    console.log(credits);
+    console.log(details);
 
-  useEffect(() => {
-    if (selectedPersonId) {
-      dispatch(fetchPersonCreditsRequest(selectedPersonId));
-      dispatch(fetchPersonDetailsRequest(selectedPersonId));
+    useEffect(() => {
+        if (selectedPersonId) {
+            dispatch(fetchPersonCreditsRequest(selectedPersonId));
+            dispatch(fetchPersonDetailsRequest(selectedPersonId));
+        }
+    }, [dispatch, selectedPersonId]);
+
+    if (loading) {
+        return <Loading />;
     }
-  }, [dispatch, selectedPersonId]);
 
-  if (loading) {
-    return <Loading />;
-  }
+    if (error) {
+        return <Error />;
+    }
 
-  if (error) {
-    return <Error />;
-  }
-
-  return (
-    <>
-      <StyledPersonDetails>
-        <>{details.biography}</>
-        <CastPerson>
-          <Title>Movies - cast</Title>
-          <MovieList>
-            <MovieTile />
-          </MovieList>
-        </CastPerson>
-        <CrewPerson>
-          <Title>Movies - crew</Title>
-          <MovieList>
-            <MovieTile />
-          </MovieList>
-        </CrewPerson>
-      </StyledPersonDetails>
-    </>
-  );
+    return (
+        <>
+            <StyledPersonDetails>
+                <PersonTile>
+                    <Picture src={`${basicImageUrl}${details.profile_path}`} />
+                    <Main>
+                        <PersonTitle>{details.name}</PersonTitle>
+                        <PersonBirthDetails>
+                            <PersonInfo>
+                                Date of birth:
+                                <PersonBirthData>{details.birthday}</PersonBirthData>
+                            </PersonInfo>
+                            <PersonInfo>
+                                Place of birth:
+                                <PersonBirthData>{details.place_of_birth}</PersonBirthData>
+                            </PersonInfo>
+                        </PersonBirthDetails>
+                        <Biography>{details.biography}</Biography>
+                    </Main>
+                </PersonTile>
+                <CastPerson>
+                    <Title>Movies - cast</Title>
+                    <MovieList>
+                        <MovieTile />
+                    </MovieList>
+                </CastPerson>
+                <CrewPerson>
+                    <Title>Movies - crew</Title>
+                    <MovieList>
+                        <MovieTile />
+                    </MovieList>
+                </CrewPerson>
+            </StyledPersonDetails>
+        </>
+    );
 };
--- src/features/PersonDetails/styled.js
+++ src/features/PersonDetails/styled.js
@@ -3,6 +3,107 @@ import styled from "styled-components";
 export const StyledPersonDetails = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
+`;
+
+export const PersonTile = styled.article`
+    background-color: ${({ theme }) => theme.color.white};
+    max-width: 1368px;
+    padding: 40px;
+    margin-top: 60px;
+    align-content: start;
+    justify-content: start;
+    display: grid;
+    grid-gap: 0px 40px;
+    grid-template-columns: 0fr 3fr;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        padding: 16px;
+        grid-gap: 0px 16px;
+        grid-template-columns: 1fr 5fr;
+    }
+`;
+
+export const Picture = styled.img`
+    width: 399px;
+    height: 564px;
+    border-radius: 5px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        width: 116px;
+        height: 163px;
+    }
+`;
+
+export const Main = styled.div`
+    padding: 8px 0px 24px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        padding: 4px 0px 4px 0px;
+        gap: 16px;
+    }
+`;
+
+export const PersonTitle = styled.h1`
+    font-size: 36px;
+    font-weight: 600;
+    line-height: 43.2px;
+    margin-bottom: 24px;
+    margin-top: 0px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 18.2px;
+        margin-bottom: 12px;
+    }
+`;
+
+export const PersonBirthDetails = styled.div`
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 21.6px;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 24px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        font-size: 12px;
+        line-height: 14.4px;
+        flex-direction: column;
+    }
+`;
+
+export const PersonInfo = styled.p`
+    color: ${({ theme }) => theme.color.stormGray};
+    margin: 0px 10px 3px 0px;
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    gap: 11px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        display: flex;
+        margin-right: 4px;
+        gap: 8px;
+    }
+`;
+
+export const PersonBirthData = styled.p`
+    color: ${({ theme }) => theme.color.black};
+    margin: 0px 10px 3px 0px;
+`;
+
+export const Biography = styled.p`
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 32px;
+    margin: 0px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        font-size: 12px;
+        line-height: 22.4px;
+    }
 `;
 
 export const CastPerson = styled.div`
